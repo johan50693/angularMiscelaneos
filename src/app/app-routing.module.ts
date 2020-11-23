03d78408e895dfefd8ddd4385446b04f0e86a80e
Albert <johan50693@gmail.com>
2020-11-23 10:30:27 -0400

    Se agrega archivo para separar rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
 import {UsuarioComponent} from './components/usuario/usuario.component'
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { 
     path: 'usuario/:id', 
     component: UsuarioComponent,
-    children: [
-      {path: 'detalle', component: UsuarioDetalleComponent},
-      {path: 'editar', component: UsuarioEditarComponent},
-      {path: 'nuevo', component: UsuarioNuevoComponent},
-      { path: '**', pathMatch: 'full', redirectTo:'nuevo'}
-    ]
+    children: USUARIO_ROUTES
   },
   { path: '**', pathMatch: 'full', redirectTo:'home'}
 ];
