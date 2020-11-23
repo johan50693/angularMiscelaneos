c4ed78a3211f278db122fb9304adbe3db7ede13a
Albert <johan50693@gmail.com>
2020-11-23 11:42:05 -0400

    obtener parametro de ruta padre desde el hijo

--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -11,7 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { 
+    console.log("Ruta Padre Usuarios Nuevo");
+    route.parent.params.subscribe( params => {
+      console.log(params);
+    });
+  }
 
   ngOnInit() {
   }
